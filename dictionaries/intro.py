72aa8e58b4017924d7393df8bdad6c99f67af5d3
Lebogang Mabala <lebogang@technanimals.com>
2020-05-19 21:04:33 +0200

    Add one step barcode price

--- dictionaries/intro.py
+++ dictionaries/intro.py
@@ -15,7 +15,7 @@ print(prices['apple'])
 barcode = '100'
 product = barcodes[barcode]
 price = prices[product]
-
+print(prices[barcodes[barcode]])
 print(price)
 prices['banana'] = 5.0
 prices['apple'] = 5.0
