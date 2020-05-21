794e071acbc5189e30dd3af2344695f951a277a2
Lebogang Mabala <lebogang@technanimals.com>
2020-05-21 21:03:16 +0200

    Add booleans

---
+++ booleans/intro.py
@@ -0,0 +1,14 @@
+# print(True)
+# print(False)
+
+a = 2.0
+b = 2
+c = 5
+print(b != a and c < 20)
+print(c < 20 or b != a)
+print((c < 20 or b != a) and b > 17)
+print(True and b > 17)
+print(b != a and b > 17 or c < 20) 
+print(False or c < 20) 
+print(c < 20 or b != a and b > 17)
+print(c < 20 or False)
