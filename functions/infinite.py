1d7d0cf2907df9967a8e997a4993c17b3b39b86c
Lebogang Mabala <lebogang@technanimals.com>
2020-06-15 21:01:30 +0200

    Add search, enumerate and zip

--- functions/infinite.py
+++ functions/infinite.py
@@ -9,9 +9,52 @@ def calculate(*numbers):
     return total
 # some_itr = 'ABC' and key = 'B', return 1
 # some_itr = [1,2,3,4,5] and key = 6, return -1
-def search(some_itr, key):
+def search_one(some_itr, key):
     # Return the position of the key in
     # some_itr or -1 when key is not found
-    pass
+    size = len(some_itr)
+    for i in range(size):
+        value = some_itr[i]
+        if value == key:
+            return i
+    return -1
 
-print(calculate(1,2,3))
+def search_two(some_itr, key):
+    # Return the position of the key in
+    # some_itr or -1 when key is not found
+    i = 0
+    for value in some_itr:
+        if value == key:
+            return i
+        i += 1
+    return -1
+
+def search_three(some_itr, key):
+    # Return the position of the key in
+    # some_itr or -1 when key is not found
+    i = 0
+    result = -1
+    for value in some_itr:
+        if value == key:
+            result = i
+            break
+        i += 1
+    return result
+
+def search_four(some_itr, key):
+    # Return the position of the key in
+    # some_itr or -1 when key is not found
+    items = enumerate(some_itr)
+    for i,value in items:
+        if value == key:
+            return i
+    return -1
+
+numbers = [1,2,3,4,5]
+key = 2
+# print(search_one(numbers, key))
+# print(search_two(numbers, key))
+# print(search_three(numbers, key))
+# print(search_four(numbers, key))
+items = zip('ABC', [1,2,2,50], (1,2,3,4,5))
+print(list(items))
