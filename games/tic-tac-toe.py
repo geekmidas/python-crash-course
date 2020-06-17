4f53963e6f336b27eba4b8d9948e42fb5a924db1
Lebogang Mabala <lebogang@technanimals.com>
2020-06-17 21:04:41 +0200

    Incomplete tic tac toe

---
+++ games/tic-tac-toe.py
@@ -0,0 +1,17 @@
+# slots = list(range(9))
+# player_one = input(': ')
+
+def draw_state(slots):
+    # Draw the slots to the screen
+    # |x|o|x|
+    # |x|o|-|
+    # |o|x|x|
+    pass
+
+def play(slots, i, marker):
+    # Place marker in the correct position
+    pass
+
+def tic_tac_toe():
+    # Game logic
+    pass
