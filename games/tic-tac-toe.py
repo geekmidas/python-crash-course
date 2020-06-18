154866bb4ff6be0c9db2441bda966770d1da9227
Lebogang Mabala <lebogang@technanimals.com>
2020-06-18 21:01:03 +0200

    Add helper functions for tic tac toe game

--- games/tic-tac-toe.py
+++ games/tic-tac-toe.py
@@ -1,16 +1,35 @@
-# slots = list(range(9))
-# player_one = input(': ')
-
+# def draw_state_matrix(slots):
+#     for row in slots:
+#         line = '|'
+#         for marker in row:
+#             line += marker + '|'
+#         print(line)
 def draw_state(slots):
-    # Draw the slots to the screen
-    # |x|o|x|
-    # |x|o|-|
-    # |o|x|x|
-    pass
+    for row in slots:
+        line = '|'+'|'.join(row)+'|'
+        print(line)
 
-def play(slots, i, marker):
+def generate_initial(size = 3):
+    matrix = []
+    for _ in range(size):
+        row = []
+        for _ in range(size):
+            row.append('-')
+        matrix.append(row)
+    return matrix
+
+def play(slots, marker):
     # Place marker in the correct position
-    pass
+    move = input('Place your marker at (x,y): ')
+    x,y = move.split(',')
+    i,j = int(x),int(y)
+    slot = slots[i - 1][j - 1]
+    if slot == '-':
+        slots[i - 1][j - 1] = marker
+        draw_state(slots)
+    else:
+        play(slots, marker)
+    # draw_state(slots)
 
 def tic_tac_toe():
     # Game logic
